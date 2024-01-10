b961b60b95421eba2df3c60f51e2625201445836
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2024-01-10 18:21:45 +0530

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
-export const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:8081";
+export const baseUrl = import.meta.env.VITE_VERCEL_BASE_URL || "http://localhost:8081";
 
-export const apikey = import.meta.env.VITE_API_KEY || "abcd";
+export const apikey = import.meta.env.VITE_VERCEL_API_KEY || "abcd";
