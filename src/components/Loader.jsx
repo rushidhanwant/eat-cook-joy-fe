2ae72647e8eb64c710b1d8587a9cbcc114852500
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2024-01-10 10:05:38 +0530

    loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="border-t-4 border-gray-400 rounded-full animate-spin h-12 w-12"></div>
+    </div>
+  );
+};
+
+export default Loader;
